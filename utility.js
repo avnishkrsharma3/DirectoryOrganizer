dbb6fb38969bd779da1bdea2db16a2323e1974f8
Avnish Kumar <avnishkrsharma3@gmail.com>
2021-02-21 07:11:44 +0530

    adding utility file

---
+++ utility.js
@@ -0,0 +1,7 @@
+module.exports.types = {
+    media: ["mp4", "mkv","MP4"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"] ,
+    photo: ['PNG','JPEG' ,'JPG','png','jpeg' ,'jpg'] 
+}
